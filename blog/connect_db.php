41065b4b57f6d23737ea3b4b14ae8b4044c78775
Phuckx97 <tienphuckx@gmail.com>
2024-09-29 10:35:11 +0700

    remove sensitive datas

--- blog/connect_db.php
+++ blog/connect_db.php
@@ -1,14 +1,27 @@
-<?php
-  define("HOST", "127.0.0.1");
-  define("USER", "544261");
-  define("PASSWORD", "tienphuckx");
-  define("DB", "544261");
-
-  function get_db_connection() {
-    $connect = new mysqli(HOST, USER, PASSWORD, DB);
-    if ($connect->connect_error) {
-      die("Connection failed: " . $connect->connect_error);
-    }
-    return $connect;
-  }
+<?php
+
+  define("HOST", "");
+
+  define("USER", "");
+
+  define("PASSWORD", "");
+
+  define("DB", "");
+
+
+
+  function get_db_connection() {
+
+    $connect = new mysqli(HOST, USER, PASSWORD, DB);
+
+    if ($connect->connect_error) {
+
+      die("Connection failed: " . $connect->connect_error);
+
+    }
+
+    return $connect;
+
+  }
+
 ?>
